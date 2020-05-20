2cccf4c893e6883755572064b23d45a312d4a933
aditya9970 <54310516+aditya9970@users.noreply.github.com>
2020-05-20 12:45:16 +0530

    signup validation implemented

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const blogRoutes = require('./routes/blog');
+const authRoutes = require('./routes/auth');
 
 //app
 const app = express();
@@ -28,6 +29,7 @@ if (process.env.NODE_ENV === 'development') {
 
 //routes middlewear
 app.use('/api', blogRoutes);
+app.use('/api', authRoutes);
 
 
 
